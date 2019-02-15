97b71edb08bad5a0ff9d054bef6d863c508fae61
Andrey Beletsky <andrey@vr2.net>
2019-02-16 03:04:50 +0700

    Fix lbry.go import paths

--- routes/index_test.go
+++ routes/index_test.go
@@ -9,8 +9,8 @@ import (
 	"strings"
 	"testing"
 
-	"github.com/lbryio/lbry.go/errors"
-	ljsonrpc "github.com/lbryio/lbry.go/jsonrpc"
+	"github.com/lbryio/lbry.go/extras/errors"
+	ljsonrpc "github.com/lbryio/lbry.go/extras/jsonrpc"
 	lbryschema "github.com/lbryio/types/go"
 	"github.com/mitchellh/mapstructure"
 	"github.com/shopspring/decimal"
