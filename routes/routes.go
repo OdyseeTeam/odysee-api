cae8896e57af5f499a2ea163ed611e0ffd1f66ed
Andrey Beletsky <andrey@vr2.net>
2019-03-15 17:11:09 +0700

    Add sentry logging

--- routes/routes.go
+++ routes/routes.go
@@ -1,12 +1,16 @@
 package routes
 
-import "github.com/gorilla/mux"
+import (
+	raven "github.com/getsentry/raven-go"
+	"github.com/gorilla/mux"
+)
 
 // InstallRoutes sets up global API handlers
 func InstallRoutes(r *mux.Router) {
+	rh := raven.RecoveryHandler
 	r.HandleFunc("/", Index)
-	r.HandleFunc("/api/proxy", Proxy)
-	r.HandleFunc("/api/proxy/", Proxy)
-	r.HandleFunc("/content/claims/{uri}/{claim}/{filename}", ContentByClaimsURI)
-	r.HandleFunc("/content/url", ContentByURL)
+	r.HandleFunc("/api/proxy", rh(Proxy))
+	r.HandleFunc("/api/proxy/", rh(Proxy))
+	r.HandleFunc("/content/claims/{uri}/{claim}/{filename}", rh(ContentByClaimsURI))
+	r.HandleFunc("/content/url", rh(ContentByURL))
 }
