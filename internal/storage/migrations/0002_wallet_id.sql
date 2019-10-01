ab93f85d4288697a59ca3b42bfe227eceb925587
Andrey Beletsky <andrey@vr2.net>
2019-10-02 01:53:30 +0700

    Fix wallet migration backwards compatibility

--- internal/storage/migrations/0002_wallet_id.sql
+++ internal/storage/migrations/0002_wallet_id.sql
@@ -6,10 +6,10 @@ ALTER TABLE "users"
     DROP COLUMN "public_key",
     DROP COLUMN "seed",
 
-    ADD COLUMN "wallet_id" varchar NOT NULL,
+    ADD COLUMN "wallet_id" varchar NOT NULL DEFAULT '',
 
-    ADD CONSTRAINT wallet_id_unique UNIQUE ("wallet_id"),
-    ALTER COLUMN "sdk_account_id" DROP NOT NULL;
+    ALTER COLUMN "sdk_account_id" DROP NOT NULL
+;
 -- +migrate StatementEnd
 
 -- +migrate Down
@@ -20,8 +20,6 @@ ALTER TABLE "users"
     ADD COLUMN "public_key" varchar NOT NULL DEFAULT '',
     ADD COLUMN "seed" varchar NOT NULL DEFAULT '',
 
-    DROP COLUMN "wallet_id",
-
-    -- DROP CONSTRAINT wallet_id_unique,
-    ALTER COLUMN "sdk_account_id" SET NOT NULL;
+    DROP COLUMN "wallet_id"
+;
 -- +migrate StatementEnd
