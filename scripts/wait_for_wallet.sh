0c01e49f72a706865e46945bd6f494d41aede198
Andrey Beletsky <andrey@vr2.net>
2019-11-15 11:04:10 -0500

    Add verbosity to the wait_for_wallet script

--- scripts/wait_for_wallet.sh
+++ scripts/wait_for_wallet.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+echo "Waiting for wallet server '${LW_LBRYNET}'..."
+
 while [[ ! $(curl -sd '{"method": "status"}' ${LW_LBRYNET} |grep '"wallet": true') ]]; do
     sleep 1
 done
+
+
+echo "Wallet server is alive"
