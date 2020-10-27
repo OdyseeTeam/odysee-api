01ce4af95c504b99897c634b1439e207021c53d4
Andrey Beletsky <andrey@vr2.net>
2020-10-27 14:08:55 +0700

    Add cache test

---
+++ app/wallet/cache_test.go
@@ -0,0 +1,43 @@
+package wallet
+
+import (
+	"testing"
+
+	"github.com/lbryio/lbrytv/app/sdkrouter"
+	"github.com/lbryio/lbrytv/internal/metrics"
+	"github.com/lbryio/lbrytv/internal/test"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"github.com/volatiletech/sqlboiler/boil"
+)
+
+func TestCache(t *testing.T) {
+	setupTest()
+
+	metricValue := metrics.GetCounterValue(metrics.AuthTokenCacheHits)
+
+	token := "abc"
+	cachedUser := currentCache.get(token)
+	assert.Nil(t, cachedUser)
+	assert.Equal(t, metricValue, metrics.GetCounterValue(metrics.AuthTokenCacheHits))
+
+	user, err := getOrCreateLocalUser(boil.GetDB(), dummyUserID, logger.Log())
+	require.NoError(t, err)
+
+	srv := test.RandServerAddress(t)
+	rt := sdkrouter.New(map[string]string{"a": srv})
+	// url, cleanup := dummyAPI(srv)
+	// defer cleanup()
+
+	err = assignSDKServerToUser(boil.GetDB(), user, rt.LeastLoaded(), logger.Log())
+	require.NoError(t, err)
+
+	currentCache.set(token, user)
+	cachedUser = currentCache.get(token)
+	assert.Equal(t, metricValue+1, metrics.GetCounterValue(metrics.AuthTokenCacheHits))
+	assert.Equal(t, user.LbrynetServerID, cachedUser.LbrynetServerID)
+	cachedUser.ID = 111111
+	cachedUser = currentCache.get(token)
+	assert.Equal(t, user.ID, cachedUser.ID)
+}
