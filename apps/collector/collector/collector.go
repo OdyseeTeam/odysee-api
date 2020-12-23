7d48398c67b67d05c04de1705e76541a68ed2fc2
Alex Grintsvayg <grin@lbry.com>
2020-12-23 13:16:22 -0500

    redo collector db

--- apps/collector/collector/collector.go
+++ apps/collector/collector/collector.go
@@ -37,11 +37,6 @@ func RouteInstaller(r *mux.Router, _ *env.Environment) {
 
 func EventHandler(w http.ResponseWriter, r *http.Request) {
 	responses.AddJSONContentType(w)
-	code := http.StatusOK
-
-	url := r.URL
-	url.Host = r.Host
-	url.Scheme = r.URL.Scheme
 
 	f, err := pkger.Open("/apps/collector/openapi.yml")
 	if err != nil {
@@ -50,15 +45,22 @@ func EventHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 	defer f.Close()
+
 	data, err := ioutil.ReadAll(f)
 	swagger, _ := openapi3.NewSwaggerLoader().LoadSwaggerFromData(data)
 	router := openapi3filter.NewRouter().WithSwagger(swagger)
+
+	url := r.URL
+	url.Host = r.Host
+	url.Scheme = r.URL.Scheme
+
 	route, pathParams, err := router.FindRoute(r.Method, url)
 	if err != nil {
 		w.WriteHeader(http.StatusBadRequest)
 		fmt.Fprintf(w, "route error: %v", err)
 		return
 	}
+
 	validator := &openapi3filter.RequestValidationInput{
 		Request:    r,
 		PathParams: pathParams,
@@ -77,18 +79,72 @@ func EventHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	event := models.Event{}
-	err = json.Unmarshal(reqBody, &event)
+	eventData := BufferingPost{}
+	err = json.Unmarshal(reqBody, &eventData)
 	if err != nil {
 		w.WriteHeader(http.StatusBadRequest)
 		fmt.Fprintf(w, "error marshaling event: %v", err)
+		return
+	}
+
+	if eventData.Type != "buffering" {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprintf(w, "unsupported event type: %s", eventData.Type)
+		return
+	}
+
+	device := eventData.Device
+	if device != models.BufferEventDeviceAndroid && device != models.BufferEventDeviceWeb {
+		device = models.BufferEventDeviceUnknown
+	}
+
+	event := models.BufferEvent{
+		URL:        eventData.Data.URL,
+		Client:     eventData.Client,
+		Device:     device,
+		Position:   eventData.Data.Position,
+		ReadyState: int16(eventData.Data.ReadyState),
+	}
+
+	if eventData.Data.Duration != nil {
+		event.Duration.SetValid(*eventData.Data.Duration)
+	}
+
+	if eventData.Data.StreamDuration != nil {
+		event.StreamDuration.SetValid(*eventData.Data.StreamDuration)
+	}
+
+	if eventData.Data.StreamBitrate != nil {
+		event.StreamBitrate.SetValid(*eventData.Data.StreamBitrate)
+	}
+
+	if eventData.Data.Player != "" {
+		event.Player.SetValid(eventData.Data.Player)
 	}
 
 	err = event.InsertG(boil.Infer())
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		fmt.Fprintf(w, "error saving event: %v", err)
+		return
 	}
 
-	w.WriteHeader(code)
+	w.WriteHeader(http.StatusOK)
+}
+
+type BufferingPost struct {
+	Type   string            `json:"type"`
+	Client string            `json:"client" `
+	Device string            `json:"device,omitempty"`
+	Data   BufferingPostData `json:"data"`
+}
+
+type BufferingPostData struct {
+	URL            string `json:"url"`
+	Position       int    `json:"position"`
+	Duration       *int   `json:"duration,omitempty"`
+	StreamDuration *int   `json:"stream_duration,omitempty"`
+	StreamBitrate  *int   `json:"stream_bitrate,omitempty"`
+	ReadyState     int    `json:"readyState"`
+	Player         string `json:"player"`
 }
