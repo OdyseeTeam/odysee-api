00e4e50f034afcc0751c897008a1fca39978a219
Andrey Beletsky <andrey@vr2.net>
2021-08-26 00:54:36 +0700

    Clean up watchman folder

--- apps/watchman/queries/queries.sql
+++
@@ -1,5 +0,0 @@
--- name: CreatePlaybackReport :exec
-
-INSERT INTO playback_reports (url, pos, por, dur, bfc, bfd, fmt, pid, cid, cdv, crt, car) VALUES (
-    $1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12
-);
