b33b0580e975a44c12cf096a8f0ba3757a6dceac
Andrey Beletsky <andrey@vr2.net>
2022-06-22 23:09:43 +0700

    Implement redis tus locker

---
+++ pkg/redislocker/redislocker_test.go
@@ -0,0 +1,50 @@
+package redislocker
+
+import (
+	"log"
+	"os"
+	"testing"
+
+	"github.com/OdyseeTeam/odysee-api/pkg/testservices"
+	"github.com/go-redis/redis/v8"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"github.com/tus/tusd/pkg/handler"
+)
+
+var redisOpts *redis.Options
+
+func TestMain(m *testing.M) {
+	var err error
+	var teardown testservices.Teardown
+	redisOpts, teardown, err = testservices.Redis()
+	if err != nil {
+		log.Fatalf("failed to init redis: %s", err)
+	}
+
+	code := m.Run()
+	if err := teardown(); err != nil {
+		log.Print(err)
+	}
+	os.Exit(code)
+}
+
+func TestLocker(t *testing.T) {
+	a := assert.New(t)
+	r := require.New(t)
+
+	locker, err := New(redisOpts)
+	r.NoError(err)
+
+	lock1, err := locker.NewLock("one")
+	a.NoError(err)
+
+	a.NoError(lock1.Lock())
+	a.ErrorIs(lock1.Lock(), handler.ErrFileLocked)
+
+	lock2, err := locker.NewLock("one")
+	a.NoError(err)
+	a.ErrorIs(lock2.Lock(), handler.ErrFileLocked)
+
+	a.NoError(lock1.Unlock())
+}
