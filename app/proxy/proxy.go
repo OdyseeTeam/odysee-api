d04f82129e9099e51ef95e29fec9a4b5dbacb8e2
Andrey Beletsky <andrey@vr2.net>
2022-07-29 05:45:49 +0700

    Remove lbrynext

--- app/proxy/proxy.go
+++ app/proxy/proxy.go
@@ -23,7 +23,6 @@ import (
 	"github.com/OdyseeTeam/odysee-api/internal/audit"
 	"github.com/OdyseeTeam/odysee-api/internal/errors"
 	"github.com/OdyseeTeam/odysee-api/internal/ip"
-	"github.com/OdyseeTeam/odysee-api/internal/lbrynext"
 	"github.com/OdyseeTeam/odysee-api/internal/metrics"
 	"github.com/OdyseeTeam/odysee-api/internal/monitor"
 	"github.com/OdyseeTeam/odysee-api/internal/responses"
@@ -137,7 +136,6 @@ func Handle(w http.ResponseWriter, r *http.Request) {
 		return nil, nil
 	}, "")
 
-	lbrynext.InstallHooks(c)
 	c.Cache = qCache
 
 	metrics.ProxyCallCounter.WithLabelValues(rpcReq.Method, c.Endpoint(), origin).Inc()
